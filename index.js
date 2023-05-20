25ffa09f4fb06fb9a6072fa9a231f70d5eeb8bd0
hasanm-tech <hasanmahmudtripto@gmail.com>
2023-05-21 02:15:59 +0600

    booking toy added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 // middleware 
@@ -28,11 +28,57 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const toyCollections = client.db('toy-troops').collection('toys')
+    const bookingCollection = client.db('toy-troops').collection('bookings')
+
+
+    app.get('/toys', async (req,res) => {
+        const result = await toyCollections.find().toArray() 
+        res.send(result)
+    })
+
+
+    app.get('/toys/:id', async (req,res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const options = {
+            projection : {
+                picture:1, name:1, price:1, category:1,rating:1, quantity:1, description:1
+            }
+        }
+        const result = await toyCollections.findOne(query,options)
+        res.send(result)
+    })
+
+
+    // bookings 
+
+    app.post('/bookings', async (req,res) => {
+        const booking = req.body;
+        console.log(booking)
+        const result = await bookingCollection.insertOne(booking)
+        res.send(result)
+    })
+
+
+    app.get('/bookings', async (req,res) => {
+        const result = await bookingCollection.find().toArray()
+        res.send(result)
+    })
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
