6bb5cf673d193980ecfc9935f8ed341ba0bda4ea
hasanm-tech <hasanmahmudtripto@gmail.com>
2023-05-21 19:45:25 +0600

    some data changes

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("successfully connected to MongoDB!");
   } finally {
   }
 }
